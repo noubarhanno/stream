f93ee2e5802764a2bd0066eba37ed14d094397bf
Noubar Jack Hanno <noubar@Noubars-MacBook-Pro.local>
2019-05-15 07:12:59 +0400

    Create Modal.js that solved the issue of rendering Modal in Deep nested components and the z-index issue

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,36 +1,49 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { fetchStream, editStream } from '../../actions';
-import StreamForm from './StreamForm';
-import _ from 'lodash';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
+import _ from "lodash";
 
 class StreamEdit extends React.Component {
-    componentDidMount = () =>{
-        this.props.fetchStream(this.props.match.params.id);
-    }
+  componentDidMount = () => {
+    this.props.fetchStream(this.props.match.params.id);
+  };
 
-    onSubmit = (formValues) => {
-        this.props.editStream(this.props.match.params.id, formValues);
-    }
+  onSubmit = formValues => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
 
-    render(){
-        if (!this.props.stream){
-            return <div>Loading ...</div>;
-        }
-        return (
-            <div>
-                <h3>Edit Stream</h3>
-                <StreamForm initialValues={_.pick(this.props.stream, 'title', 'description')} onSubmit={this.onSubmit} />
-                {/* the initial value could be {{title: this.props.stream.title, description:this.props.stream.description}} */}
-            </div>
-        );
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading ...</div>;
     }
+    if (
+      this.props.stream.userId !== this.props.userId &&
+      this.props.stream.userId
+    ) {
+      return <div>You dont have a permission</div>;
+    }
+    return (
+      <div>
+        <h3>Edit Stream</h3>
+        <StreamForm
+          initialValues={_.pick(this.props.stream, "title", "description")}
+          onSubmit={this.onSubmit}
+        />
+        {/* the initial value could be {{title: this.props.stream.title, description:this.props.stream.description}} */}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state, ownProps) => {
-    return {
-        stream: state.stream[ownProps.match.params.id]
-    };
-}
+  return {
+    stream: state.stream[ownProps.match.params.id],
+    userId: state.auth.userId
+  };
+};
 
-export default connect(mapStateToProps,{fetchStream, editStream})(StreamEdit);
+export default connect(
+  mapStateToProps,
+  { fetchStream, editStream }
+)(StreamEdit);
