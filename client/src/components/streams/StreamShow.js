f5304577b55c1530f30b3c5a98bfedb10ebeba30
Noubar Jack Hanno <noubar@Noubars-MacBook-Pro.local>
2019-05-15 09:59:59 +0400

    we've done the StreamShow Component and we show only text without streaming video

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component{
-    render(){
-        return(
-            <div>StreamShow</div>
+    componentDidMount(){
+        this.props.fetchStream(this.props.match.params.id);    
+    }
+
+    renderStream(){
+        
+        if (!this.props.stream){
+            return <div>Loading ...</div>;
+        }
+        const {title, description} = this.props.stream
+        return (
+            <div>
+                <h2>{title}</h2>
+                <h5>{description}</h5>
+            </div>
         );
     }
-};
 
-export default StreamShow;
+    render(){
+        return(
+            <div>
+            {this.renderStream()}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state,ownProps) => {
+    return { stream: state.stream[ownProps.match.params.id]};
+}
+
+
+export default connect(mapStateToProps,{ fetchStream })(StreamShow);
